87e240d7657aa5a7829ce2ea4121181b0ccf5fd6
fufufunfufunfu <nakamura798@gmail.com>
2019-10-01 14:34:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const server = app.listen(3000, function() {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+const list = [
+  {
+    id: 1,
+    name: "taro"
+  },
+  {
+    id: 2,
+    name: "jiro"
+  }
+];
+
+app.get("/api/user/list", function(req, res, next) {
+  res.json(list);
+  console.log("accessed");
+});
